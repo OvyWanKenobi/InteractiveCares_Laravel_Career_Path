ab3ce4b0e8ef6ae4af2cce7cb209ff54811e0bb2
OvyWanKenobi <imovy1997@gmai.com>
2024-11-07 00:57:45 +0600

    assignment 10 complete

--- Laravel_Career_Path/Assignments/Assignment_10_RESTFUL_API/routes/web.php
+++ Laravel_Career_Path/Assignments/Assignment_10_RESTFUL_API/routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use App\Http\Controllers\Web;
-
+use Dedoc\Scramble\Scramble;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return redirect()->to('/docs/api#/');
+});
 
 
 // Route::get('/{link:short_url_code}', [Web\V1\LinkController::class, 'visitShortLink']);
 
 Route::get('/{link:short_url_code}', [Web\V2\LinkController::class, 'visitShortLink']);
+
+
+
